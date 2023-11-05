9d036b1328ce4e548f6970fd094da3a8840c5dfa
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-11-05 14:54:23 +0100

    added: open solution on project launch

---
+++ Editor/IDE/OpenSolutionOnStartup.cs
@@ -0,0 +1,59 @@
+// Copyright (C) 2021-2023 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+#if UNITY_EDITOR
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Editor.Tools.IDE
+{
+	/// <summary>
+	/// Will launch the IDE associated with .sln files when the Unity project is opened.
+	/// </summary>
+	[InitializeOnLoad] public class OpenSolutionOnStartup
+	{
+		private const String SessionStateKey = "CodeSmile.Editor.Tools.IsProjectLaunching";
+		[NotNull] private static String ProjectRootPath => $"{Application.dataPath}/..";
+
+		private static void TryOpenSolution()
+		{
+			var solutionUrl = TryGetSolutionPath();
+			if (File.Exists(solutionUrl))
+			{
+				Debug.Log($"Welcome {Environment.UserName}! Your IDE should now open the project's solution.\n" +
+				          $"Solution path: {solutionUrl}");
+				Application.OpenURL(solutionUrl);
+			}
+		}
+
+		private static String TryGetSolutionPath()
+		{
+			var solutions = Directory.GetFiles(ProjectRootPath, "*.sln");
+			return solutions.Length > 0 ? solutions[0] : null;
+		}
+
+		private static Boolean IsProjectLaunching()
+		{
+			var launching = SessionState.GetBool(SessionStateKey, true);
+			if (launching)
+				SessionState.SetBool(SessionStateKey, false);
+
+			return launching;
+		}
+
+		private static void OnDelayCall()
+		{
+			EditorApplication.delayCall -= OnDelayCall; // not necessary but it's a good habit
+
+			if (IsProjectLaunching())
+				TryOpenSolution();
+		}
+
+		// delayed in case .sln file has not been generated yet
+		static OpenSolutionOnStartup() => EditorApplication.delayCall += OnDelayCall;
+	}
+}
+#endif
